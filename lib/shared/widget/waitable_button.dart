b1bd7469c5bf68f2cd2fb9a7e40ae9e67a00306e
Tetsukick <endeavor.0329@gmail.com>
2024-05-18 07:30:59 +0700

    add waitable button

---
+++ lib/shared/widget/waitable_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class WaitableElevatedButton extends StatefulWidget {
+
+  WaitableElevatedButton({
+    required this.onPressed,
+    required this.child,
+    this.style,
+    super.key,
+  });
+  @override
+  createState() => _WaitableElevatedButtonState();
+
+  final AsyncCallback? onPressed;
+  final ButtonStyle? style;
+  final Widget child;
+}
+
+class _WaitableElevatedButtonState extends State<WaitableElevatedButton> {
+  bool _waiting = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: widget.style,
+      onPressed: widget.onPressed == null || _waiting
+          ? null
+          : () async {
+        setState(() => _waiting = true);
+        await widget.onPressed!();
+        setState(() => _waiting = false);
+      },
+      child: widget.child,
+    );
+  }
+}
